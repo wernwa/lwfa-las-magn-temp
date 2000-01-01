1272125584a64ffc19f948f30f020e9e7a250069
Walter Werner <wernwa@gmail.com>
2000-01-01 13:01:26 +1030

    comments and loadet

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 
 cycle_temperature = 80.0
-tty_driver = '/dev/pts/8'
-#tty_driver = '/dev/ttyACM0'
+#tty_driver = '/dev/pts/8'
+tty_driver = '/dev/ttyACM0'
 baud = 115200
 serial_timeout = 1 # sec
 
 
+# assignment of the temperatur sensor index to the magnet EPICs name
 name_to_index = {
-    'q1:temp': 4,
+    'q1:temp': 0,
     'q2:temp': 1,
     'q3:temp': 2,
     'q4:temp': 3,
-    'q5:temp': 0,
+    'q5:temp': 4,
     'q6:temp': 5,
     'q7:temp': 6,
     'd1:temp': 7,
