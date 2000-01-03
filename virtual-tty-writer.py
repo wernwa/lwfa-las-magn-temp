8b07d320b78fe5a8ed9d33aea68ccc8f65b1f9a8
Walter Werner <wernwa@gmail.com>
2000-01-03 13:57:57 +1030

    virtual writer socat

--- virtual-tty-writer.py
+++ virtual-tty-writer.py
@@ -5,21 +5,28 @@ import random
 import time
 
 # system call to socat:
-#socat -d -d pty,raw,echo=1 pty,raw,echo=1
+#
+# socat -d -d pty,raw,echo=1 pty,raw,echo=1
+#2000/01/03 12:20:52 socat[2185] N PTY is /dev/pts/3
+#2000/01/03 12:20:52 socat[2185] N PTY is /dev/pts/9
+#2000/01/03 12:20:52 socat[2185] N starting data transfer loop with FDs [3,3] and [5,5]
+#
+# The first pty is for writing (virtual-tty-writer.py)
+# the second is for reading (setup.py)
+pty_nr=3
 
 
-tty_name='/dev/pts/16'
+s = serial.Serial('/dev/pts/3')
 
-
-s = serial.Serial(tty_name)
-
+temp=[0,0,0,0,0,0,0,0,0]
 
 while True:
     line = ''
-    for i in range(1,10):
-        temp=random.random()
-        line+='%.2f '%temp
-        time.sleep(0.5)
+    for i in range(0,len(temp)):
+        if random.random()>0.5: sign=1 
+        else: sign=-1
+        temp[i]=temp[i]+sign*random.random()
+        line+='%.2f '%temp[i]
     line+='\n'
-    print line
     s.write(line)
+    time.sleep(0.5)
