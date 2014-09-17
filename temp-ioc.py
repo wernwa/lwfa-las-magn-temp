d956fb9284b120bcf7e2eb2a8d285684cddb2922
Walter Werner <wernwa@gmail.com>
2014-09-17 19:36:25 +0200

    temp 1-7 in record vars

--- temp-ioc.py
+++ temp-ioc.py
@@ -11,7 +11,37 @@ pvdb = {
         'prec' : 3,
         'scan' : 1,
     },
-    'T1' : {
+    'M1:T' : {
+        'prec' : 3,
+        'low' : -20, 'high': 70,
+        'lolo': -30,'hihi': 99,
+    },
+    'M2:T' : {
+        'prec' : 3,
+        'low' : -20, 'high': 70,
+        'lolo': -30,'hihi': 99,
+    },
+    'M3:T' : {
+        'prec' : 3,
+        'low' : -20, 'high': 70,
+        'lolo': -30,'hihi': 99,
+    },
+    'M4:T' : {
+        'prec' : 3,
+        'low' : -20, 'high': 70,
+        'lolo': -30,'hihi': 99,
+    },
+    'M5:T' : {
+        'prec' : 3,
+        'low' : -20, 'high': 70,
+        'lolo': -30,'hihi': 99,
+    },
+    'M6:T' : {
+        'prec' : 3,
+        'low' : -20, 'high': 70,
+        'lolo': -30,'hihi': 99,
+    },
+    'M7:T' : {
         'prec' : 3,
         'low' : -20, 'high': 70,
         'lolo': -30,'hihi': 99,
@@ -22,6 +52,7 @@ pvdb = {
         'states': [Severity.NO_ALARM, Severity.MAJOR_ALARM]
     },
 }
+tempcnt = 7
 
 class myDriver(Driver):
     def  __init__(self):
@@ -38,10 +69,20 @@ class myDriver(Driver):
         return value
 
     def read_tty(self):
+        global tempcnt
         while True:
             line = self.ser.readline()
             t_arr = line.split(' ')
-            self.setParam('T1', t_arr[0])
+            if (len(t_arr)!=tempcnt+1):
+                continue
+            #print t_arr
+            self.setParam('M1:T', t_arr[0])
+            self.setParam('M2:T', t_arr[1])
+            self.setParam('M3:T', t_arr[2])
+            self.setParam('M4:T', t_arr[3])
+            self.setParam('M5:T', t_arr[4])
+            self.setParam('M6:T', t_arr[5])
+            self.setParam('M7:T', t_arr[6])
             self.updatePVs()
 
 if __name__ == '__main__':
