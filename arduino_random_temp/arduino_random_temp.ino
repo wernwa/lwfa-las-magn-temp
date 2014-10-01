e8abc43050b853495b17a20fb499000319e39071
Walter Werner <wernwa@gmail.com>
2014-10-01 22:52:08 +0930

    9 temps, code optimisation

--- arduino_random_temp/arduino_random_temp.ino
+++ arduino_random_temp/arduino_random_temp.ino
@@ -1,4 +1,5 @@
-float t[7];
+float t[9];
+int cnt=9;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -6,15 +7,22 @@ void setup() {
   Serial.begin(115200);
   // make the pushbutton's pin an input:
   //pinMode(pushButton, INPUT);
+  
+  // init sensors to roum temperature
+  for (int i=0;i<cnt;i++){
+    t[i] = 20.0;
+  }
+  
+  
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  for (int i=0;i<7;i++){
+  for (int i=0;i<cnt;i++){
     long randNumber = random(0, 100);
-    int v=1;
-    if(random(0,2) == 1){ v=-1;};
-    t[i] = (float)randNumber / 100.0 + v*t[i]; 
+    float v=1.0;
+    if(random(0,2) >= 1){ v=-1.0;};
+    t[i] = (float)v*randNumber / 100.0 + t[i]; 
     Serial.print(t[i]);
     Serial.print(" ");
   }
--- temp-ioc.py
+++ temp-ioc.py
@@ -6,119 +6,78 @@ import thread
 import serial
 import sys
 
+
 prefix = 'shicane:'
-pvdb = {
-#    'RAND' : {
-#        'prec' : 3,
-#        'scan' : 1,
-#    },
-    'q1:temp' : {
-        'prec' : 3,
-	'unit' : 'C',
-#        'low' : -20, 'high': 70,
-        'lolo': -30,'hihi': 99,
-    },
-    'q2:temp' : {
-        'prec' : 3,
-        'low' : -20, 'high': 70,
-        'lolo': -30,'hihi': 99,
-    },
-    'q3:temp' : {
-        'prec' : 3,
-        'low' : -20, 'high': 70,
-        'lolo': -30,'hihi': 99,
-    },
-    'q4:temp' : {
-        'prec' : 3,
-        'low' : -20, 'high': 70,
-        'lolo': -30,'hihi': 99,
-    },
-    'q5:temp' : {
-        'prec' : 3,
-        'low' : -20, 'high': 70,
-        'lolo': -30,'hihi': 99,
-    },
-    'q6:temp' : {
-        'prec' : 3,
-        'low' : -20, 'high': 70,
-        'lolo': -30,'hihi': 99,
-    },
-    'q7:temp' : {
-        'prec' : 3,
-        'low' : -20, 'high': 70,
-        'lolo': -30,'hihi': 99,
-    },
-#    'STATUS' : {
-#        'type' : 'enum',
-#        'enums':  ['OK', 'ERROR'],
-#        'states': [Severity.NO_ALARM, Severity.MAJOR_ALARM]
-#    },
-}
+pvdb={}
+for name in ['q1','q2','q3','q4','q5','q6','q7','d1','d2']:
+	pvdb['%s:temp'%name] = {
+			'prec' : 2,
+			'unit' : 'C',
+			'low' : -20, 'high': 70,
+			'lolo': -30,'hihi': 99,
+		}
 tempcnt = 7
 
 class myDriver(Driver):
-    def  __init__(self):
-        super(myDriver, self).__init__()
-        self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+	def  __init__(self):
+		global tempcnt
+		super(myDriver, self).__init__()
+		self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
-	# throw first lines away (i.a. not from the start)
-	const=False
-	tempcnt_prev=0
-	tempcnt_prev2=0
-	while const==False:
-		line = self.ser.readline()
-		sys.stdout.write(line)
-		t_arr = line.split(' ')
+		# throw first lines away (i.a. not from the start)
+		const=False
+		tempcnt_prev=0
+		tempcnt_prev2=0
+		while const==False:
+			line = self.ser.readline()
+			sys.stdout.write(line)
+			t_arr = line.split(' ')
+			tempcnt = len(t_arr)-1
+			if tempcnt == tempcnt_prev and tempcnt==tempcnt_prev2: const=True
+			tempcnt_prev=tempcnt
+			tempcnt_prev2=tempcnt_prev
+
+	
+		#t_arr = line.split(' ')
 		tempcnt = len(t_arr)-1
-		if tempcnt == tempcnt_prev and tempcnt==tempcnt_prev2: const=True
-		tempcnt_prev=tempcnt
-		tempcnt_prev2=tempcnt_prev
+		print '%d temperature sensors recognized'%tempcnt
+		print '------------------------------'
+		print ' Start polling (CTRL+C -> end).'
+		print '------------------------------'
+		self.tid = thread.start_new_thread(self.read_tty,())
 
-	
-	#t_arr = line.split(' ')
-	#tempcnt = len(t_arr)-1
-	print '%d temperature sensors recognized'%tempcnt
-	
-        self.tid = thread.start_new_thread(self.read_tty,())
-	print '------------------------------'
-	print 'Start polling (CTRL+C -> end).'
-	print '------------------------------'
+	def read(self, reason):
+		#if reason == 'RAND':
+		#    value = random.random()
+		#else:
+		value = self.getParam(reason)
+		return value
 
-    def read(self, reason):
-        if reason == 'RAND':
-            value = random.random()
-        else:
-            value = self.getParam(reason)
-        return value
 
-    def read_tty(self):
-        global tempcnt
-        while True:
-            line = self.ser.readline()
-	    #print line
-            t_arr = line.split(' ')
-            if (len(t_arr)!=tempcnt+1):
-                continue
-            #print t_arr
-            self.setParam('q1:temp', t_arr[0])
-            self.setParam('q2:temp', t_arr[1])
-            self.setParam('q3:temp', t_arr[2])
-            self.setParam('q4:temp', t_arr[3])
-            self.setParam('q5:temp', t_arr[4])
-            self.setParam('q6:temp', t_arr[5])
-            self.setParam('q7:temp', t_arr[6])
-            self.updatePVs()
+	def read_tty(self):
+
+		global tempcnt
+ 		record_list=['q1:temp','q2:temp','q3:temp','q4:temp','q5:temp','q6:temp','q7:temp','d1:temp','d2:temp']
+
+		while True:
+			line = self.ser.readline()
+			#print line
+			t_arr = line.split(' ')
+			#print t_arr
+			if (len(t_arr)!=tempcnt+1):
+				continue
+
+			for i in range(0,tempcnt):
+				self.setParam(record_list[i],t_arr[i])
+			self.updatePVs()
 
 if __name__ == '__main__':
-    server = SimpleServer()
-    server.createPV(prefix, pvdb)
-    driver = myDriver()
+	server = SimpleServer()
+	server.createPV(prefix, pvdb)
+	driver = myDriver()
 
-    # process CA transactions
-    while True:
-        server.process(0.1)
-
-    while True:
+	# process CA transactions
+	while True:
 		try:
 			server.process(0.1)
 		except KeyboardInterrupt:
