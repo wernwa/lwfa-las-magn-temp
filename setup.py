44bc22c23d992ae6fd1ba1d7218e7ca18ba37bf4
Walter Werner <wernwa@gmail.com>
2014-12-17 07:51:19 -0800

    setup name to index adapted

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ serial_timeout = 1 # sec
 
 # assignment of the temperatur sensor NR to the magnet EPICs name
 name_to_index = {
-    'q1:temp': 1,
-    'q2:temp': 2,
-    'q3:temp': 3,
-    'q4:temp': 4,
-    'q5:temp': 5,
-    'q6:temp': 6,
-    'q7:temp': 7,
-    'd1:temp': 8,
-    'd2:temp': 9,
+    'q1:temp': 3,
+    'q2:temp': 4,
+    'q3:temp': 5,
+    'q4:temp': 6,
+    'q5:temp': 7,
+    'q6:temp': 8,
+    'q7:temp': 9,
+    'd1:temp': 1,
+    'd2:temp': 2,
 }
